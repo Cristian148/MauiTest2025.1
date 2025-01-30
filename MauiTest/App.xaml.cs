a3bd41d91e09fb72904a9838ce3c31ff2813b0bf
Cristian <caal25@hotmail.com>
2025-01-30 15:37:49 -0300

    Creación proyecto básico

---
+++ MauiTest/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new AppShell();
+
+            var NPage = new NavigationPage(new ContentPageDemo());
+            NPage.BarBackgroundColor = Colors.Chocolate;
+            NPage.BarTextColor = Colors.White;
+        }
+    }
+}
